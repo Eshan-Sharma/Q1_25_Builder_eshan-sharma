a5b197d552a1107aa801571776c237d153479509
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-01-22 17:33:59 +0530

    added withdraw fn

--- vault/programs/vault/src/lib.rs
+++ vault/programs/vault/src/lib.rs
@@ -16,6 +16,9 @@ pub mod vault {
     pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
         ctx.accounts.deposit(amount)
     }
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount)
+    }
 }
 
 #[derive(Accounts)]
@@ -63,6 +66,36 @@ impl<'info> Deposit<'info> {
         };
 
         let cpi_ctx = CpiContext::new(cpi_program, cpi_account);
+        transfer(cpi_ctx, amount)
+    }
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(seeds=[b"state",signer.key().as_ref()],bump=vault_state.state_bump)]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(seeds=[b"vault",vault_state.key().as_ref()],bump=vault_state.vault_bump)]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+impl<'info> Withdraw<'info> {
+    pub fn withdraw(&mut self, amount: u64) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_account = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.signer.to_account_info(),
+        };
+
+        let seeds = &[
+            &[self.vault_state.to_account_info().key().as_ref()],
+            &[&[self.vault_state.vault_bump]],
+        ];
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_account, signer_seeds);
         transfer(cpi_ctx, amount)
     }
 }
