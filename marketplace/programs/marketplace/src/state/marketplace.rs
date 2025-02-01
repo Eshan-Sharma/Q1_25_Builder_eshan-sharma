433f5fb56badae0383b76f3e71b6c8db9df77441
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-02-01 20:53:07 +0530

    fix naming

--- marketplace/programs/marketplace/src/state/marketplace.rs
+++ marketplace/programs/marketplace/src/state/marketplace.rs
@@ -6,7 +6,7 @@ pub struct Marketplace {
     pub fee: u16,
     pub bump: u8,
     pub treasury_bump: u8,
-    pub rewards_bump: u8,
+    pub rewards_mint_bump: u8,
     pub name: String, //Limit set to 32 bytes
 }
 
