88d427e74c63ea62799bcba4d4743afb3f7faded
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-02-02 17:47:26 +0530

    add delisting function

---
+++ marketplace/programs/marketplace/src/contexts/delist.rs
@@ -0,0 +1,65 @@
+use anchor_lang::prelude::*;
+
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::metadata::{Metadata,MasterEditionAccount,MetadataAccount};
+use anchor_spl::token_interface::{transfer_checked,TransferChecked,Mint,TokenAccount,TokenInterface};
+
+use crate::state::Listing;
+use crate::state::Marketplace;
+
+#[derive(Accounts)]
+pub struct Delist<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+    #[account(
+        seeds=[b"marketplace",marketplace.name.as_str().as_bytes()],
+        bump=marketplace.bump
+    )]
+    pub marketplace: Account<'info, Marketplace>,
+    pub maker_mint:InterfaceAccount<'info,Mint>,
+    #[account(
+        mut, 
+        associated_token::mint=maker_mint,
+        associated_token::authority=maker,
+    )]
+    pub maker_ata : InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint=maker_mint,
+        associated_token::authority=listing,
+    )]
+    pub vault:InterfaceAccount<'info,TokenAccount>,
+    pub associated_token_program:Program<'info, AssociatedToken>,
+    pub system_program:Program<'info, System>,
+    pub token_program:Interface<'info, TokenInterface>,
+
+    #[account(
+        mut, 
+        close=maker,
+        seeds=[marketplace.key().as_ref(),maker_mint.key().as_ref()],
+        bump=listing.bump
+    )]
+    pub listing:Account<'info, Listing>,
+}
+
+impl<'info> Delist<'info>{
+    pub fn withdraw_nft(&mut self)->Result<()>{
+        let seeds = &[
+            &self.marketplace.key().to_bytes()[..],
+            &self.maker_mint.key().to_bytes()[..],
+            &[self.listing.bump],
+        ];
+        let signer_seeds = &[&seeds[..]];
+
+        let accounts = TransferChecked{
+            from:self.vault.to_account_info(),
+             mint:self.maker_mint.to_account_info(),
+             to:self.maker_ata.to_account_info(),
+             authority:self.listing.to_account_info()
+        };
+
+        let cpi_ctx = CpiContext::new_with_signer(self.token_program.to_account_info(), accounts, signer_seeds);
+        transfer_checked(cpi_ctx, 1, self.maker_mint.decimals)?;
+        Ok(())
+    }
+}
--- marketplace/programs/marketplace/src/contexts/mod.rs
+++ marketplace/programs/marketplace/src/contexts/mod.rs
@@ -3,3 +3,9 @@ pub use init::*;
 
 pub mod list;
 pub use list::*;
+
+pub mod purchase;
+pub use purchase::*;
+
+pub mod delist;
+pub use delist::*;
