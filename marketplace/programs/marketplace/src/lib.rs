176de5fff7741eccc186b311da7df29766738c38
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-02-02 17:55:48 +0530

    add instructions

--- marketplace/programs/marketplace/src/lib.rs
+++ marketplace/programs/marketplace/src/lib.rs
@@ -1,17 +1,34 @@
 use anchor_lang::prelude::*;
 pub mod contexts;
+pub use contexts::*;
 pub mod state;
+pub use state::*;
 declare_id!("Cst1bMdLbmrAdYMdnCWmaJceDSnjuhNNw6JMVPwJe4uo");
 
 #[program]
 pub mod marketplace {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.init(fee, &ctx.bumps, name)?;
+        Ok(())
+    }
+
+    pub fn listing(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()?;
+        Ok(())
+    }
+    pub fn purchase(ctx: Context<Purchase>) -> Result<()> {
+        ctx.accounts.send_sol()?;
+        ctx.accounts.send_nft()?;
+        ctx.accounts.close_mint_vault()?;
+        Ok(())
+    }
+
+    pub fn delisting(ctx: Context<Delist>) -> Result<()> {
+        ctx.accounts.withdraw_nft()?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
