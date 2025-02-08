93cd08cb8ae78149b57bb7b4c882c966d524b050
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-02-08 21:47:51 +0530

    init dice betting game

---
+++ dice/programs/dice/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7sQSfqt7sJ96D1zBK3GgPTj5hpWWMc5cJoGDpLPCtwbg");
+
+#[program]
+pub mod dice {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
