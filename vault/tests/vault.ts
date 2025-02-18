558161bc634d9bd11bb35a3eb970bb625497ffb7
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-02-18 08:07:21 +0530

    test: deposit sol

--- vault/tests/vault.ts
+++ vault/tests/vault.ts
@@ -1,16 +1,34 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Vault } from "../target/types/vault";
+import {
+  PublicKey,
+  SystemProgram,
+  Keypair,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import { assert } from "chai";
 
 describe("vault", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  let provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.Vault as Program<Vault>;
 
-  it("Is initialized!", async () => {
+  let signer = anchor.web3.Keypair.generate();
+  let vaultStatePda: PublicKey;
+  let vaultPda: PublicKey;
+  let vaultBump: number;
+  let stateBump: number;
+  let depositAmount = 1 * LAMPORTS_PER_SOL;
+
+  it("Fund the signer account", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const airdrop = await provider.connection.requestAirdrop(
+      signer.publicKey,
+      2 * depositAmount
+    );
+    await provider.connection.confirmTransaction(airdrop);
+    const balance = await provider.connection.getBalance(signer.publicKey);
+    assert.equal(balance, 2 * LAMPORTS_PER_SOL, "Signer should have 2 SOL");
   });
 });
