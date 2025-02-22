8703f8ef34ad0847f9e109166afd1e7237e01e6d
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-02-22 22:12:03 +0530

    better structuring

--- capstone_project/regen-credit/programs/regen-credit/src/state/carbon_credit.rs
+++ capstone_project/regen-credit/programs/regen-credit/src/state/carbon_credit.rs
@@ -1,83 +1,40 @@
 use anchor_lang::prelude::*;
 
+pub use crate::helper::Country;
+pub use crate::helper::EnergyUnits;
+pub use crate::helper::SourceType;
+
 #[account]
 pub struct CarbonCredit {
     pub country: Country,
-    pub value: u32, //Max 4,294,967,295.
-    pub units: EnergyUnits,
     pub source_type: SourceType,
-    pub original_carbon_credits: u16,      //Max 65535
-    pub remaining_carbon_credits: u16,     //Max 65535
+    pub units: EnergyUnits,
+
+    pub value: u32, //Max 4,294,967,295.
+
+    pub original_carbon_credits: u16,  //Max 65535
+    pub remaining_carbon_credits: u16, //Max 65535
+
     pub grid_emission_factor: u32,         //Max 4,294,967,295.
     pub grid_emission_factor_decimals: u8, //Max 255
+
     pub listed: bool,
     pub maker: Pubkey,
     pub price_per_carbon_credit: u16, //Max 65535
     pub bump: u8,
 }
 impl Space for CarbonCredit {
-    const INIT_SPACE: usize = 8 + (1 + 4 + 1 + 1 + 2 + 2 + 4 + 1 + 1 + 32 + 2 + 1);
-}
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
-pub enum SourceType {
-    Solar,
-}
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
-pub enum EnergyUnits {
-    MWh,
-    KWh,
-}
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
-pub enum Country {
-    UnitedKingdom,
-    SouthAfrica,
-    China,
-    HongKong,
-    India,
-    Indonesia,
-    Japan,
-    Korea,
-    Thailand,
-    Australia,
-    NewZealand,
-    SaudiArabia,
-    Turkey,
-    UnitedArabEmirates,
-    Canada,
-    Mexico,
-    UnitedStates,
-    Argentina,
-    Brazil,
-    Austria,
-    Belgium,
-    Bulgaria,
-    Croatia,
-    Cyprus,
-    CzechRepublic,
-    Denmark,
-    Estonia,
-    Finland,
-    France,
-    Germany,
-    Greece,
-    Hungary,
-    Iceland,
-    Ireland,
-    Italy,
-    Latvia,
-    Lithuania,
-    Luxembourg,
-    Malta,
-    Netherlands,
-    Norway,
-    Poland,
-    Portugal,
-    Romania,
-    RussianFederation,
-    Serbia,
-    Slovakia,
-    Slovenia,
-    Spain,
-    Sweden,
-    Switzerland,
+    const INIT_SPACE: usize = 8 + //Anchor Discriminator
+    1 +  //Country Enum
+    1 +  //SourceType Enum
+    1 +  //EnergyUnits Enum
+    4 +  //u32
+    2 +  //u16
+    2 +  //u16
+    4 +  //u32
+    1 +  //u8 
+    1 +  //bool
+    32 + //PubKey
+    2 +  //u16
+    1; //u8
 }
