91b0ecd81aefd71ebf08aec111a2554683b12cd9
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-02-24 13:53:53 +0530

    add errors

--- capstone_project/regen-credit/programs/regen-credit/src/errors.rs
+++ capstone_project/regen-credit/programs/regen-credit/src/errors.rs
@@ -8,4 +8,10 @@ pub enum ErrorCode {
     CalculationOverflow,
     #[msg("Incorrect Subtraction")]
     CalculationUnderflow,
+    #[msg("Value too large for u16")]
+    ValueTooLargeForU16,
+    #[msg("Value cannot be 0")]
+    ValueZero,
+    #[msg("Price cannot be 0")]
+    PriceZero,
 }
