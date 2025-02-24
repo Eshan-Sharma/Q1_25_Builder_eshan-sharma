bc548f357e5903bccb512bc761f1c07772c018d3
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-02-24 13:54:09 +0530

    test: init carbon credits

--- capstone_project/regen-credit/tests/regen-credit.ts
+++ capstone_project/regen-credit/tests/regen-credit.ts
@@ -9,12 +9,8 @@ describe("regen-credit", () => {
   const program = anchor.workspace.RegenCredit as Program<RegenCredit>;
 
   let maker = anchor.web3.Keypair.generate();
-  enum Country {
-    India = "India",
-  }
-  enum EnergyUnits {
-    KWh = "KWh",
-  }
+  let carbonCreditPda;
+
   before(async () => {
     await provider.connection.confirmTransaction(
       await provider.connection.requestAirdrop(
@@ -22,17 +18,15 @@ describe("regen-credit", () => {
         anchor.web3.LAMPORTS_PER_SOL * 20
       )
     );
+    [carbonCreditPda] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("carbon_credit"), maker.publicKey.toBuffer()],
+      program.programId
+    );
   });
 
   it("Should create a CarbonCredit Account with correct values", async () => {
-    let [carbonCreditPda, carbonCreditBump] =
-      await anchor.web3.PublicKey.findProgramAddress(
-        [Buffer.from("carbon_credit"), maker.publicKey.toBuffer()],
-        program.programId
-      );
-
     await program.methods
-      .initializeCarbonCredit([Country.India], 10, 1000, [EnergyUnits.KWh])
+      .initializeCarbonCredit({ india: {} }, 10, 1000, { kWh: {} })
       .accountsPartial({
         maker: maker.publicKey,
         carbonCredit: carbonCreditPda,
@@ -44,10 +38,9 @@ describe("regen-credit", () => {
     const carbonCredit = await program.account.carbonCredit.fetch(
       carbonCreditPda
     );
-
-    assert.equal(carbonCredit.country.toString(), "India");
+    assert.deepEqual(carbonCredit.country, { india: {} });
     assert.equal(carbonCredit.value, 1000);
-    assert.equal(carbonCredit.units.toString(), "KWh");
+    assert.deepEqual(carbonCredit.units, { kWh: {} });
     assert.equal(carbonCredit.pricePerCarbonCredit, 10);
     assert.equal(carbonCredit.listed, false);
     assert.equal(
@@ -57,20 +50,9 @@ describe("regen-credit", () => {
   });
   it("Should fail if maker is not the signer", async () => {
     const otherUser = anchor.web3.Keypair.generate();
-    const initParams = {
-      country: Country.India,
-      pricePerCarbonCredit: new anchor.BN(10),
-      value: new anchor.BN(1000),
-      units: EnergyUnits.KWh,
-    };
-    let [carbonCreditPda, carbonCreditBump] =
-      await anchor.web3.PublicKey.findProgramAddress(
-        [Buffer.from("carbon_credit"), maker.publicKey.toBuffer()],
-        program.programId
-      );
     try {
       await program.methods
-        .initializeCarbonCredit(initParams)
+        .initializeCarbonCredit({ india: {} }, 10, 1000, { kWh: {} })
         .accountsPartial({
           maker: maker.publicKey,
           carbonCredit: carbonCreditPda,
@@ -78,13 +60,43 @@ describe("regen-credit", () => {
         })
         .signers([otherUser])
         .rpc();
-      assert.fail("Expected transaction to fail");
+      assert.fail(`unknown signer`);
     } catch (err) {
-      assert.include(err.message, "ConstraintSigner");
+      assert.include(err.message, `unknown signer`);
     }
   });
-  it("Should fail if value or grid_emission_factor is zero", async () => {});
-  it("Should fail if price_per_carbon_credit is zero", async () => {});
+  it("Should fail if value or grid_emission_factor is zero", async () => {
+    try {
+      await program.methods
+        .initializeCarbonCredit({ india: {} }, 10, 0, { kWh: {} })
+        .accountsPartial({
+          maker: maker.publicKey,
+          carbonCredit: carbonCreditPda,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([maker])
+        .rpc();
+      assert.fail("Value cannot be 0");
+    } catch (err) {
+      assert.include(err.message, "Simulation failed");
+    }
+  });
+  it("Should fail if price_per_carbon_credit is zero", async () => {
+    try {
+      await program.methods
+        .initializeCarbonCredit({ india: {} }, 0, 1000, { kWh: {} })
+        .accountsPartial({
+          maker: maker.publicKey,
+          carbonCredit: carbonCreditPda,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([maker])
+        .rpc();
+      assert.fail("Price cannot be 0");
+    } catch (err) {
+      assert.include(err.message, "Simulation failed");
+    }
+  });
   it("Should fail if on-chain protocol account is not initialized", async () => {
     try {
       await program.account.carbonCredit.fetch(
