c589ebb6c36ef92a304b23bf4f2628d9b443da1c
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-02-26 23:10:55 +0530

    split purchase instruction

--- capstone_project/regen-credit/programs/regen-credit/src/context/mod.rs
+++ capstone_project/regen-credit/programs/regen-credit/src/context/mod.rs
@@ -3,9 +3,13 @@ pub mod init_marketplace;
 pub mod list;
 // pub mod make_nft;
 // pub mod purchase;
+pub mod reduce_carbon_credit;
+pub mod send_usdc;
 
 pub use init_carbon_credit::*;
 pub use init_marketplace::*;
 pub use list::*;
 // pub use make_nft::*;
 // pub use purchase::*;
+pub use reduce_carbon_credit::*;
+pub use send_usdc::*;
