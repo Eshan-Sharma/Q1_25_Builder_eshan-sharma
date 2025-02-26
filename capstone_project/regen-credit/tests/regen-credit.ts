30e5c634405374c1aa442395586add0c647f7231
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-02-26 23:13:22 +0530

    test: reducing carbon credit

--- capstone_project/regen-credit/tests/regen-credit.ts
+++ capstone_project/regen-credit/tests/regen-credit.ts
@@ -3,36 +3,148 @@ import { Program } from "@coral-xyz/anchor";
 import { RegenCredit } from "../target/types/regen_credit";
 import { assert } from "chai";
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import { PublicKey } from "@solana/web3.js";
+import {
+  createMint,
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  mintTo,
+  getOrCreateAssociatedTokenAccount,
+} from "@solana/spl-token";
 
-describe("regen-credit", () => {
+describe("RegenCredit Tests", () => {
   let provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.RegenCredit as Program<RegenCredit>;
 
   let maker = anchor.web3.Keypair.generate();
   let admin = anchor.web3.Keypair.generate();
+  let taker = anchor.web3.Keypair.generate();
+
   let carbonCreditPda;
   let marketplacePda;
   let treasuryPda;
+
+  let mint: PublicKey;
+  let treasury: PublicKey;
+  let takerUsdc;
+  let makerUsdc;
+  let adminUsdc;
+
+  // Constants
   let marketplaceName = "My Marketplace";
   let marketplaceFee = 2;
+  let pricePerCarbonCredit = 10;
+  let energyValue = 1000;
 
   before(async () => {
+    // Airdrop
     await provider.connection.confirmTransaction(
       await provider.connection.requestAirdrop(
         maker.publicKey,
         anchor.web3.LAMPORTS_PER_SOL * 20
       )
     );
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        taker.publicKey,
+        anchor.web3.LAMPORTS_PER_SOL * 20
+      )
+    );
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        admin.publicKey,
+        anchor.web3.LAMPORTS_PER_SOL * 20
+      )
+    );
+
+    //Get PDAs
     [carbonCreditPda] = await anchor.web3.PublicKey.findProgramAddress(
       [Buffer.from("carbon_credit"), maker.publicKey.toBuffer()],
       program.programId
+    );
+    [marketplacePda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("marketplace"), Buffer.from(marketplaceName)],
+      program.programId
+    );
+    [treasuryPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("treasury"), marketplacePda.toBuffer()],
+      program.programId
+    );
+    // USDC Mint
+    mint = await createMint(
+      provider.connection,
+      admin,
+      admin.publicKey,
+      null,
+      6
+    );
+    //Initialize maker's USDC account
+    makerUsdc = (
+      await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        maker, // Payer
+        mint, // USDC Mint
+        maker.publicKey // Owner of the account
+      )
+    ).address;
+    console.log("Maker USDC:", makerUsdc.toBase58());
+    // Initialize taker's USDC account
+    takerUsdc = (
+      await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        taker, // Payer
+        mint, // USDC Mint
+        taker.publicKey // Owner of the account
+      )
+    ).address;
+    console.log("Taker USDC:", takerUsdc.toBase58());
+    // Initialize taker's USDC account
+    adminUsdc = (
+      await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        admin, // Payer
+        mint, // USDC Mint
+        admin.publicKey // Owner of the account
+      )
+    ).address;
+    console.log("Admin USDC:", adminUsdc.toBase58());
+    await mintTo(
+      provider.connection,
+      taker, //signer
+      mint, //mint
+      takerUsdc, //destination
+      admin, //authority
+      1000000000 // 1000 USDC
+    );
+    await mintTo(
+      provider.connection,
+      maker, //signer
+      mint, //mint
+      makerUsdc, //destination
+      admin, //authority
+      1000000000 // 1000 USDC
+    );
+    await mintTo(
+      provider.connection,
+      admin, //signer
+      mint, //mint
+      adminUsdc, //destination
+      admin, //authority
+      1000000000 // 1000 USDC
     );
   });
 
   it("Should create a CarbonCredit Account with correct values", async () => {
     await program.methods
-      .initializeCarbonCredit({ india: {} }, 10, 1000, { kWh: {} })
+      .initializeCarbonCredit(
+        { india: {} },
+        pricePerCarbonCredit,
+        energyValue,
+        {
+          kWh: {},
+        }
+      )
       .accountsPartial({
         maker: maker.publicKey,
         carbonCredit: carbonCreditPda,
@@ -165,14 +277,6 @@ describe("regen-credit", () => {
     }
   });
   it("Should create a Marketplace Account", async () => {
-    [marketplacePda] = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("marketplace"), Buffer.from(marketplaceName)],
-      program.programId
-    );
-    [treasuryPda] = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("treasury"), marketplacePda.toBuffer()],
-      program.programId
-    );
     try {
       await program.methods
         .initializeMarketplace(marketplaceName, marketplaceFee)
@@ -195,14 +299,6 @@ describe("regen-credit", () => {
     } catch (err) {}
   });
   it("Should fail to recreate Marketplace Account", async () => {
-    [marketplacePda] = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("marketplace"), Buffer.from(marketplaceName)],
-      program.programId
-    );
-    [treasuryPda] = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("treasury"), marketplacePda.toBuffer()],
-      program.programId
-    );
     try {
       await program.methods
         .initializeMarketplace(marketplaceName, marketplaceFee)
@@ -235,5 +331,43 @@ describe("regen-credit", () => {
       listedCarbonCredits.length > 0, //Number of listed carbon credit accounts
       "Should fetch listed carbon credits"
     );
+  });
+  it("Should reduce the number of remaining_carbon_credits", async () => {
+    try {
+      let carbonCreditBefore = await program.account.carbonCredit.fetch(
+        carbonCreditPda
+      );
+
+      await program.methods
+        .reduceRemainingCredits(8)
+        .accountsPartial({
+          taker: taker.publicKey,
+          maker: maker.publicKey,
+          mint,
+
+          carbonCredit: carbonCreditPda,
+          marketplace: marketplacePda,
+          treasury: treasuryPda,
+
+          takerUsdc,
+          makerUsdc,
+          associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([taker])
+
+        .rpc({ skipPreflight: true });
+
+      let carbonCreditAfter = await program.account.carbonCredit.fetch(
+        carbonCreditPda
+      );
+      assert.equal(
+        carbonCreditAfter.remainingCarbonCredits,
+        carbonCreditBefore.remainingCarbonCredits - 8
+      );
+    } catch (err) {
+      console.log("ERROR:", err);
+    }
   });
 });
