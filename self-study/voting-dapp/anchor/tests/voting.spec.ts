1a9278e52546ca5e8d74e5c0a0d1a8637192f5d8
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-03-09 08:00:34 +0530

    test: initialize poll

---
+++ self-study/voting-dapp/anchor/tests/voting.spec.ts
@@ -0,0 +1,30 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { Voting } from "../target/types/voting";
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+
+const IDL = require("../target/idl/voting.json");
+const votingAddress = new PublicKey(
+  "coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF"
+);
+describe("Voting", () => {
+  it("Initialize Poll", async () => {
+    const context = await startAnchor(
+      "",
+      [{ name: "voting", programId: votingAddress }],
+      []
+    );
+    const provider = new BankrunProvider(context);
+
+    const votingProgram = new Program<Voting>(IDL, provider);
+    await votingProgram.methods
+      .initializePoll(
+        new anchor.BN(1),
+        new anchor.BN(0),
+        new anchor.BN(1841481814),
+        "What is your favorite peanut butter?"
+      )
+      .rpc();
+  });
+});
