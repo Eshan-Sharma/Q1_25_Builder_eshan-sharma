756c95b935a4cf51c429aa58d747e805cd028bcf
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-04-03 08:54:57 +0530

    create vesting program

---
+++ self-study/token-vesting/anchor/programs/tokenvesting/src/lib.rs
@@ -0,0 +1,67 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
+
+declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+
+#[program]
+pub mod tokenvesting {
+    use super::*;
+
+    pub fn create_veting_account(
+        ctx: Context<CreateVestingAccount>,
+        company_name: String,
+    ) -> Result<()> {
+        *ctx.accounts.vesting_account = VestingAccount {
+            owner: ctx.accounts.signer.key(),
+            token_mint: ctx.accounts.mint.key(),
+            treasury_token_account: ctx.accounts.treasury_token_account.key(),
+            company_name,
+            bump: ctx.bumps.vesting_account,
+            treasury_bump: ctx.bumps.treasury_token_account,
+        };
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(company_name:String)]
+pub struct CreateVestingAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + VestingAccount::INIT_SPACE,
+        seeds = [company_name.as_bytes()],
+        bump
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+    pub system_program: Program<'info, System>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        init,
+        token::mint=mint,
+        token::authority=vesting_account,
+        payer = signer,
+        seeds = [company_name.as_bytes(), b"vesting_treasury".as_ref()],
+        bump
+    )]
+    pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub token_mint: Pubkey,
+    pub treasury_token_account: Pubkey,
+    #[max_len(50)]
+    pub company_name: String,
+    pub bump: u8,
+    pub treasury_bump: u8,
+}
