d73bece8e4868bc4b33b63df38744da71221e8f9
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-04-03 10:57:16 +0530

    create employee account

--- self-study/token-vesting/anchor/programs/tokenvesting/src/lib.rs
+++ self-study/token-vesting/anchor/programs/tokenvesting/src/lib.rs
@@ -9,7 +9,7 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 pub mod tokenvesting {
     use super::*;
 
-    pub fn create_veting_account(
+    pub fn create_vesting_account(
         ctx: Context<CreateVestingAccount>,
         company_name: String,
     ) -> Result<()> {
@@ -24,6 +24,25 @@ pub mod tokenvesting {
 
         Ok(())
     }
+    pub fn create_employee_account(
+        ctx: Context<CreateEmployeeAccount>,
+        start_time: i64,
+        end_time: i64,
+        cliff_time: i64,
+        total_amount: u64,
+    ) -> Result<()> {
+        *ctx.accounts.employee_account = EmployeeAccount {
+            beneficiary: ctx.accounts.beneficiary.key(),
+            start_time,
+            end_time,
+            cliff_time,
+            vesting_account: ctx.accounts.vesting_account.key(),
+            total_amount,
+            total_withdrawn: 0,
+            bump: ctx.bumps.employee_account,
+        };
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -35,7 +54,7 @@ pub struct CreateVestingAccount<'info> {
         init,
         payer = signer,
         space = 8 + VestingAccount::INIT_SPACE,
-        seeds = [company_name.as_bytes()],
+        seeds = [company_name.as_ref()],
         bump
     )]
     pub vesting_account: Account<'info, VestingAccount>,
@@ -54,6 +73,24 @@ pub struct CreateVestingAccount<'info> {
     pub token_program: Interface<'info, TokenInterface>,
 }
 
+#[derive(Accounts)]
+pub struct CreateEmployeeAccount<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub beneficiary: SystemAccount<'info>,
+    #[account(has_one = owner)]
+    pub vesting_account: Account<'info, VestingAccount>,
+    #[account(
+        init,
+        payer = owner,
+        space = 8 + EmployeeAccount::INIT_SPACE,
+        seeds = [b"employee_vesting", beneficiary.key().as_ref(),vesting_account.key().as_ref()],
+        bump
+    )]
+    pub employee_account: Account<'info, EmployeeAccount>,
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct VestingAccount {
@@ -65,6 +102,20 @@ pub struct VestingAccount {
     pub bump: u8,
     pub treasury_bump: u8,
 }
+
+#[account]
+#[derive(InitSpace)]
+pub struct EmployeeAccount {
+    pub beneficiary: Pubkey,
+    pub start_time: i64,
+    pub end_time: i64,
+    pub cliff_time: i64,
+    pub vesting_account: Pubkey,
+    pub total_amount: u64,
+    pub total_withdrawn: u64,
+    pub bump: u8,
+}
+
 #[error_code]
 pub enum ErrorCode {
     #[msg("Claiming is not available yet.")]
